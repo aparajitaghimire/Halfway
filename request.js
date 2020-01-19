bc16c9045d3f327d833362d9a47e0e37f93dc746
Aparajita Ghimire <46252271+aparajitaghimire@users.noreply.github.com>
2020-01-19 13:49:40 -0500

    Update request.js

--- request.js
+++ request.js
@@ -3,6 +3,7 @@ var ids = [];
 var genreIds = [];
 var movieImgs = [];
 var descriptions = [];
+var ratings = [];
 var inputGenre1 = 16;
 var inputGenre2 = 35;
 var inputGenre3 = 0;
@@ -37,21 +38,34 @@ request.onload = function() {
        if (request.status >= 200 && request.status < 400) {
             let i = 0;
             //data.forEach(results => {
-            for (i = 0; i < 5; i++) {  
+            for (i = 0; i < 10; i++) {  
                 const card = document.createElement('div')
                 card.setAttribute('class', 'card')
                 const h1 = document.createElement('h1')
                 h1.textContent = data.results[i].title
 
+                titles.push(data.results[i].title)
+                descriptions.push(data.results[i].overview)
+                ratings.push(data.results[i].vote_average);
                 const p = document.createElement('p')
+              
                 //movie.description = 
                 let des = data.results[i].overview
-      p.textContent = `${des}`
+                let rat = data.results[i].vote_average
+                p.textContent = `${des}`
+              
                 container.appendChild(card)
                 card.appendChild(h1)
                 card.appendChild(p)
             }
-            //})
+
+            //sort based on avg vote?
+            let j = 0;
+            for(j = 0; j < 10; j++) {
+                document.getElementById("Title" + '' + j).innerHTML = titles[j];
+                document.getElementById("Description" + '' + j).innerHTML = descriptions[j];
+                
+            }
         } else {
             const errorMessage = document.createElement('marquee')
             errorMessage.textContent = `Gah, it's not working!`
@@ -59,4 +73,3 @@ request.onload = function() {
         }
 }
 request.send()
-
