99fdb402d026a3e8c238221ec78f8488bf91b16c
AndreaMiranda99 <57204361+AndreaMiranda99@users.noreply.github.com>
2020-01-19 10:37:02 -0500

    Add files via upload

---
+++ Page2.js
@@ -0,0 +1,12 @@
+var Genres=[];
+var counter = 1; 
+function GetGenres() {
+	var Selected_Genre = document.getElementById("Genre");
+	var x = Selected_Genre.options[Selected_Genre.selectedIndex].text;
+	Genres.push(x); 
+	document.getElementById("Number").innerHTML = "Genre Preference " + counter;
+	//delete below after
+	//document.getElementById("pTest").innerHTML = Genres.toString();
+	//document.getElementById("pTest").innerHTML = "sadasd"+Genres.toString();
+	counter++;
+}	 
